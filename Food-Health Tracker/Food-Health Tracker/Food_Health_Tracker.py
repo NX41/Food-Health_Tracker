cc60656c44adbf5a0ccc513d17fd39841a48a7a2
NX41 <arthur.tree21@bathspa.ac.uk>
2024-03-01 14:08:35 +0000

    commit 0.0.1

---
+++ Food-Health Tracker/Food-Health Tracker/Food_Health_Tracker.py
@@ -0,0 +1,19 @@
+import sqlite3
+
+from sqlite3 import Error
+
+# Database Generation
+cx = sqlite3.connect("healthdiet.db")
+
+
+
+cx.close()
+# Health Tracker
+
+# Recipe Planner
+
+# User Interface
+ 
+# Health IO
+
+# Pantry IO    
